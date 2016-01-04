320b2ace34ed6c06a1f2a988f500678e4885b7f5
Alexander A. Novikov <mem-memov@yandex.ru>
2016-01-04 05:52:45 +0000

    clause API

--- api/clause/create/index.php
+++ api/clause/create/index.php
@@ -9,13 +9,38 @@ if (isset($_REQUEST['json'])) {
     $request = $_REQUEST;
 }
 
-if (isset($request)) {
+if (!isset($request['predicate'])) {
+    $response = [
+        'error' => 'predicate parameter missing'
+    ];
+}elseif (!isset($request['question'])) {
+    $response = [
+        'error' => 'question parameter missing'
+    ];
+}elseif (!isset($request['answer'])) {
+    $response = [
+        'error' => 'answer parameter missing'
+    ];
+} else {
+    $predicateWord = $syntax->provideWord($request['predicate']);
+    $predicate = $predicateWord->providePredicate();
     
+    $questionWord = $syntax->provideWord($request['question']);
+    $question = $questionWord->provideQuestion();
+    
+    $answerWord = $syntax->provideWord($request['answer']);
+    $answer = $answerWord->provideAnswer();
+    
+    $argument = $syntax->provideArgument($predicate, $question);
+    
+    $compliment = $syntax->provideCompliment($argument, $answer);
+    
+    $clause = $syntax->createClause([$compliment]);
+    
+    $response = [
+        'id' => $clause->getId()
+    ];
 }
-
-$response = [
-    'clauseId' => $clause->getId()
-];
 
 
 header('Content-Type: text/html; charset=utf-8');
